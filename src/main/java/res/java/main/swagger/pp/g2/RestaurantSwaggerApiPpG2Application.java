d3c66260b42bb66cbbcbecb4f5f563637bdb1faa
youraleangseng <youra.leangseng@gmail.com>
2016-07-29 22:39:34 +0700

    addWebservice Project

---
+++ src/main/java/res/java/main/swagger/pp/g2/RestaurantSwaggerApiPpG2Application.java
@@ -0,0 +1,12 @@
+package res.java.main.swagger.pp.g2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantSwaggerApiPpG2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantSwaggerApiPpG2Application.class, args);
+	}
+}
---
+++ src/test/java/res/java/main/swagger/pp/g2/RestaurantSwaggerApiPpG2ApplicationTests.java
@@ -0,0 +1,16 @@
+package res.java.main.swagger.pp.g2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestaurantSwaggerApiPpG2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
